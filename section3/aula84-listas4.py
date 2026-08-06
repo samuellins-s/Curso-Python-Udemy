884d170b3a3078df0e9ee7fe4f4cacbd7d1e69c4
samuellins <rnsamueldlins@gmail.com>
2026-08-06 12:08:54 -0300

    aula 84

---
+++ section3/aula84-listas4.py
@@ -0,0 +1,17 @@
+'''
+extend -> estende a lista
++ -> concatena listas
+
+'''
+# + (concatenação das listas)
+
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+
+lista_c = lista_a + lista_b 
+print(lista_c)
+
+# extend -> método usado na própria lista
+
+lista_a.extend(lista_b)
+print(lista_a)
