0d6707da8d5f4d775d64ff08ad146f810acc653e
samuellins <rnsamueldlins@gmail.com>
2026-08-08 15:49:35 -0300

    Create aula98-opternaria.py

---
+++ section3/aula98-opternaria.py
@@ -0,0 +1,7 @@
+'''
+Operação ternária (condição de uma linha)
+<valor> if <condição> else <outro valor>
+
+'''
+
+variavel = 'Valor' if False else 'Outro valor' # printa Valor se verdadeiro, se falso, printa Outro valor
