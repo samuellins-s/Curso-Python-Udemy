e9ac78a06d8cc5b5667ccce425967c0224b45ffe
samuellins <rnsamueldlins@gmail.com>
2026-08-08 21:37:52 -0300

    Update aula98-opternaria.py

--- section3/aula98-opternaria.py
+++ section3/aula98-opternaria.py
@@ -5,3 +5,13 @@ Operação ternária (condição de uma linha)
 '''
 
 variavel = 'Valor' if False else 'Outro valor' # printa Valor se verdadeiro, se falso, printa Outro valor
+
+condicao = 10 == 10
+variavel = 'Valor' if condicao else 'Outro valor'
+print(variavel)
+
+# ultimos dois dígitos do CPF
+
+digito = 9
+novo_digito = digito if digito <= 9 else 0 # printar digito se menor igual que 9, se não 0
+print(novo_digito)
