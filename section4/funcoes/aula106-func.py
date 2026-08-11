de685a61f8f64d2f25c80b53e59ad9f9eb7f97cc
samuellins <rnsamueldlins@gmail.com>
2026-08-11 18:56:47 -0300

    Create aula106-func.py

---
+++ section4/funcoes/aula106-func.py
@@ -0,0 +1,12 @@
+'''
+Introdução às funções (def)
+Funções são trechos de código usados para replicar determinada ação ao longo do seu código.
+Elas podem receber valores para parâmetros (argumentos) e retornar um valor específico.
+Por padrão, funções Python retornam None (nada)
+
+'''
+
+def Printar():
+    print('Python')
+
+Printar() # chamada da função
