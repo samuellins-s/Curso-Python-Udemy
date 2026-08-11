fb8cd4a65badd48b677f192904587a2bd7f0847e
samuellins <rnsamueldlins@gmail.com>
2026-08-11 18:50:43 -0300

    Create aula99#EX.py

---
+++ section3/aula99#EX.py
@@ -0,0 +1,12 @@
+'''
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF
+multiplicando cada um dos valores por uma contagem regressiva começando de 10
+
+    Exemplo:   746.824.890-70 (746824890)
+            10  9   8   7   6   5   4   3   2
+    *       7   4   6   8   2   4   8   9   0
+            70  36  48  56  12  20  32  27  0
+
+            
+'''
