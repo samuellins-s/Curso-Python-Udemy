6f3ec19674b6fa07d140c9780f66cc99faa69d01
samuellins <rnsamueldlins@gmail.com>
2026-08-12 15:13:19 -0300

    Update aula106-func.py

--- section4/funcoes/aula106-func.py
+++ section4/funcoes/aula106-func.py
@@ -10,3 +10,15 @@ def Printar():
     print('Python')
 
 Printar() # chamada da função
+
+def imprimir(a, b, c): # a, b e c são parâmetros (variáveis a serem definidas)
+    print(a, b, c)
+
+imprimir(1, 2, 3) # argumentos (valor da variável)
+imprimir(4, 5, 6)
+
+def saudacao(nome='Marian'): # se não houver argumento na chamada de função, será preechido por Marian
+    print(f'Olá, {nome}!')
+
+saudacao('Samuel')
+saudacao()
