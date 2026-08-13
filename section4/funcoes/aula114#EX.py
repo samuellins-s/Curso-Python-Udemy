1e810bd0a46b40fc1a755afb5730e73d661e53f8
samuellins-ifrn <rnsamueldlins@gmail.com>
2026-08-13 16:15:32 -0300

    Create aula114#EX.py

---
+++ section4/funcoes/aula114#EX.py
@@ -0,0 +1,30 @@
+'''
+Exercicios com funções:
+
+Crie uma função que multiplica todos os argumentos não nomeados recebidos.
+
+Retorne o valor para uma variável e mostre o valor da variavel.
+
+Crie uma função fala se um numero é par ou impar.
+Retorne se o número e par o ímpar.
+
+'''
+
+def multiplicar_valores(*args):
+    total = 1
+    for numero in args:
+        total *= numero
+    return total
+
+resultado_multiplicacao = multiplicar_valores(1, 2, 3, 4, 5, 6)
+print(resultado_multiplicacao)
+
+def par_ou_impar(numero):
+    numero = int(numero)
+
+    if numero % 2 == 0:
+        return (f'O numero {numero} é par.')
+    else: 
+        return (f'O numero {numero} é ímpar.')
+
+print(par_ou_impar(2))
