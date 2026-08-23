639439a3aa29efecda3343ae8437914acbc14da4
samuellins <rnsamueldlins@gmail.com>
2026-08-23 16:35:37 -0300

    Update aula121-dict.py

--- section4/dict/aula121-dict.py
+++ section4/dict/aula121-dict.py
@@ -1,5 +1,23 @@
 '''
 Dicionários (dict)
 Par "Chave" e "Valor".
+Usar "{}" ou classe dict para criar dicionário
+São mutáveis
 
 '''
+
+pessoa = {
+    'nome': 'Samuel',
+    'sobrenome': 'Lins',
+    'idade': 18,
+    'altura': 1.72,
+    'endereços': [
+        {'rua': 'tal tal', 'número': 123},
+        {'rua': 'tal tal', 'número': 123},
+    ],
+}
+
+print(pessoa['nome']) # acesso pelo indice (string)
+
+for chave in pessoa: # no indice direto (i) -> pega as chaves
+    print(chave, pessoa[chave]) # para pegar os valores (pessoa[chave])
