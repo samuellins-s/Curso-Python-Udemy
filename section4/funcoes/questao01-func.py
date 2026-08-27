451c4ece07eb5c0bc52e4b3e1d0bdcb765fb89d6
samuellins-ifrn <rnsamueldlins@gmail.com>
2026-08-27 09:34:08 -0300

    Create questao01-func.py

---
+++ section4/funcoes/questao01-func.py
@@ -0,0 +1,14 @@
+'''
+calcular desconto:
+    1. definir o preco do produto
+    2. definir o valor do desconto
+    3. fazer a operação final ()
+
+'''
+
+def calcular_desconto(preco, desconto):
+    preco = 
+    desconto = 
+    return 
+
+print(calcular_desconto())
